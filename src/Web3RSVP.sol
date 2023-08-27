590d4d41d2d4cd79561c360e783c14230942ace4
jojomensah89 <jojomensah89@gmail.com>
2023-08-27 08:54:50 +0000

    updated the contract

--- src/Web3RSVP.sol
+++ src/Web3RSVP.sol
@@ -1,8 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9;
 
-// Uncomment this line to use console.log
-import "hardhat/console.sol";
 
 contract Web3RSVP {
     event NewEventCreated(
